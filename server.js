7deba2cc15218ac9fd494a2fd8178f4a5c25e5a8
she1018 <she020108@gmail.com>
2024-01-17 15:54:14 +0900

    1-7. MongoDB와 서버 연결하려면

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const app = express();
 // "/public" 경로에 있는 파일들을 html에서 사용 가능하도록 함
 app.use(express.static(__dirname + "/public"));
 
+// MongoDB 연결하는 코드
+const { MongoClient } = require("mongodb");
+
+let db;
+const url =
+  "mongodb+srv://she020108:ebP2mtLOblNyvZni@cluster0.dtbvib6.mongodb.net/?retryWrites=true&w=majority";
+new MongoClient(url)
+  .connect() // MongoDB에 접속
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum"); // "forum"이라는 이름의 db에 접속
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // 내 컴퓨터에서 8080 PORT 오픈
 // PORT: 다른 컴퓨터에서 내 컴퓨터에 접속할 수 있는 통로
 // "http://IPv4주소:PORT번호"로 내 컴퓨터에 접속 가능
@@ -20,8 +36,9 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// "http:/localhost:8080/hello" 에 접속 시 "Hello" 표시
+// "/hello"로 접속 시 db의 "post" collection에 document 1개 추가
 app.get("/hello", (req, res) => {
+  db.collection("post").insertOne({ date: Date.now() });
   res.send("Hello");
 });
 
